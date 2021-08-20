0e16356b1bc50fbc94fc402f5bd10cb1e9216bd9
Sakshi <sakshishetty1231997@gmail.com>
2021-08-20 20:40:52 +0530

    Added: Method to push elements to the stack

--- src/com/bridgelabz/StackMain.java
+++ src/com/bridgelabz/StackMain.java
@@ -1,12 +1,18 @@
 package com.bridgelabz;
 /**
- * Purpose - To implement Stack and Queue Operations
+ * Purpose - To create a Stack and push elements to the stack
+ * Final sequence to be printed - 56 - 30 - 70
  * @author - Sakshi Shetty
  * @version - 16.0
  * @since - 2021-08-20
  */
 public class StackMain {
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Program");
+		MyStack stack = new MyStack();
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		stack.display();
+
 	}
 }
