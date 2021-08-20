590d1e83b893aff529f1d90b46ccd464b035699a
Sakshi <sakshishetty1231997@gmail.com>
2021-08-20 20:25:49 +0530

    Added: Welcome message to the program

---
+++ src/com/bridgelabz/StackMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/**
+ * Purpose - To implement Stack and Queue Operations
+ * @author - Sakshi Shetty
+ * @version - 16.0
+ * @since - 2021-08-20
+ */
+public class StackMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Program");
+	}
+}
