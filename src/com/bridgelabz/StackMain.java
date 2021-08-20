a113757af4684c0b63200c744d6690d9dc447505
Sakshi <sakshishetty1231997@gmail.com>
2021-08-20 21:47:35 +0530

    Refactor: Modified the multiline comments

--- src/com/bridgelabz/StackMain.java
+++ src/com/bridgelabz/StackMain.java
@@ -1,7 +1,6 @@
 package com.bridgelabz;
 /**
- * Purpose - To create a Stack and push elements to the stack
- * Final sequence to be printed - 56 - 30 - 70
+ * Purpose - To create a Stack and perform peak and pop operations
  * @author - Sakshi Shetty
  * @version - 16.0
  * @since - 2021-08-20
